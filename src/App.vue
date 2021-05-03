0bf242f4c428d93e993504c8929386de92ce4055
HolaSoyGonzalo <daved.pacco@gmail.com>
2021-05-04 00:57:57 +0200

    Button+Sorting Functions + small styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <JobList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order By Title</button>
+        <button @click="handleClick('salary')">Order By Salary</button>
+        <button @click="handleClick('location')">Order By Location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -8,11 +16,13 @@
 import { defineComponent, ref } from "vue";
 import JobList from "./components/JobsList.vue";
 import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
   components: { JobList },
   setup() {
+    //Comments are just just  old examples while learning TS
     // const state = reactive({
     //   name: "Gonzalo",
     //   age: 26 as string | number,
@@ -21,7 +31,6 @@ export default defineComponent({
     // return { ...toRefs(state) };
     // const name = ref("Gonzalo");
     // const age = ref<number | string>(25);
-
     // return { name, age };
 
     const jobs = ref<Job[]>([
@@ -31,9 +40,16 @@ export default defineComponent({
       { title: "Hard Worker", location: "Venice", salary: 400, id: "4" },
       { title: "Workaholic", location: "Bergamo", salary: 500, id: "5" },
     ]);
-    return { jobs };
+
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
   },
 
+  //Comments are just just  old examples while learning TS
   // methods: {
   //   changeName(name: string) {
   //     this.name = name;
@@ -47,4 +63,21 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} £$€</p>
@@ -20,17 +21,27 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, computed } from "vue";
 import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
 export default defineComponent({
   props: {
     jobs: {
-      type: Array as PropType<Job[]>,
       required: true,
+      type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
     },
   },
   setup(props) {
-    console.log(props.jobs);
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
   },
 });
 </script>
